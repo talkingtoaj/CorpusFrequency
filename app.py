810085712f96aa30c5e893aece2a2cf232f9eb90
Michael Ellis <anz5michael@gmail.com>
2020-06-24 08:30:34 +1000

    choosing phrases

--- app.py
+++ app.py
@@ -1,42 +1,78 @@
 from flask import Flask, render_template, request
 from get_ngrams import results
 from find_phrases import search
-import pickle
+import pickle, csv
 app = Flask(__name__)
 
-state = {}
+ngram_groups = {}
 try:
-    with open("state", "rb") as file:
-        state = pickle.load(file)
+    with open("ngram_groups", "rb") as file:
+        ngram_groups = pickle.load(file)
 except FileNotFoundError:
     for n in range(2, 7):
-        state[str(n)] = {
+        ngram_groups[str(n)] = {
             result[0]: {
                 "ngram": result[0],
                 "count": result[1],
                 "checked": True,
+                "phrases": [],
             } for result in results[str(n)]
         }
+ngrams_to_n = {}
+for n, groups in ngram_groups.items():
+    for ngram in groups:
+        ngrams_to_n[ngram] = n
 
-@app.route("/toggle", methods=["POST"])
+@app.route("/")
+def home():
+    return render_template("nav.html")
+
+@app.route("/toggle-ngram", methods=["POST"])
 def toggle():
     ngram = request.form['ngram']
-    n = request.form['n']
-    state[n][ngram]["checked"] = not state[n][ngram]["checked"]
+    n = ngrams_to_n[ngram]
+    ngram_groups[n][ngram]["checked"] = not ngram_groups[n][ngram]["checked"]
     save_state()
     return ""
 
-@app.route("/phrase/<n>")
-def hello_world(n):
-    return render_template("ngrams.html", list=state[n].values(), n=n)
+@app.route("/toggle-phrase", methods=["POST"])
+def choose():
+    ngram = request.form['ngram']
+    n = ngrams_to_n[ngram]
+    phrase = request.form['phrase']
+    phrases = ngram_groups[n][ngram]["phrases"]
+    if phrase in phrases:
+        phrases.remove(phrase)
+    else:
+        phrases.append(phrase)
+    save_state()
+    return ""
 
-@app.route("/word/<word>")
-def word(word):
-    return render_template("phrase_view.html", results=search(word))
+@app.route("/ngrams/<n>")
+def ngrams(n):
+    return render_template("ngrams.html", list=ngram_groups[n].values())
+
+@app.route("/phrase/<ngram>")
+def phrase(ngram):
+    n = ngrams_to_n[ngram]
+    return render_template("phrase_view.html", results=search(ngram), ngram=ngram, phrases=ngram_groups[n][ngram]["phrases"])
+
+@app.route("/export", methods=["POST"])
+def export():
+    with open('phrases.csv', mode='w') as csv_file:
+        fieldnames = ['ngram', 'phrase']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for group in ngram_groups.values():
+            for ngram, value in group.items():
+                for phrase in value["phrases"]:
+                    writer.writerow({'ngram': ngram, 'phrase': phrase})
+    return ""
 
 def save_state():
-    with open("state", "wb") as file:
-        pickle.dump(state, file)
+    with open("ngram_groups", "wb") as file:
+        pickle.dump(ngram_groups, file)
 
 if __name__ == "__main__":
     app.run(debug=False)
--- find_phrases.py
+++ find_phrases.py
@@ -15,9 +15,9 @@ def search(phrase):
         while True:
             location = lower_content.find(phrase, upto)
             if location == -1: break
-            results.append((
-                name, 
-                content[max(0, location-100): location + 100]
-            ))
+            results.append({
+                "fileName": name, 
+                "phrase": content[max(0, location-100): location + 100]
+            })
             upto = location + 1
     return results
